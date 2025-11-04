32f4eb487d6756a27a7ea9855a7e8f1f19dce899
LaloMalvado <latan.intelligence@gmail.com>
2025-11-04 09:54:53 -0600

    chore: trigger pages workflow (#11)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,1 @@
-// @ts-nocheck
-import "./audio.ts";
-import "./bugs.ts";
-import "./effects.ts";
-import "./hud.ts";
-import "./i18n.ts";
-import "./game.ts";   // juego actual sin cambios
+import "./game.ts";
